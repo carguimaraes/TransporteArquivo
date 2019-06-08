7f340c98cf7f7728c74249f4d70bdc426dfb18ec
carlos.GMA <carguimaraesgma@gmail.com>
2019-06-08 16:57:54 -0300

    doanload

--- src/main/java/br/com/gma/transportearquivo/Arquivo.java
+++ src/main/java/br/com/gma/transportearquivo/Arquivo.java
@@ -1,43 +1,117 @@
 package br.com.gma.transportearquivo;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
+import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 
 public class Arquivo {
-	
+
 	public void listar() {
-		String serverFtp="192.168.15.2";
-		int portFtp=21;
-		String userFtp="gma";
-		String pswFtp="123";
-		
+		String serverFtp = "192.168.15.2";
+		int portFtp = 21;
+		String userFtp = "gma";
+		String pswFtp = "123";
+
 		FTPClient ftpClient = new FTPClient();
 		try {
-			
+
 			ftpClient.connect(serverFtp, portFtp);
 			ftpClient.login(userFtp, pswFtp);
-			
+
 			FTPFile[] files = ftpClient.listFiles();
 			DateFormat dateFormater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 			for (FTPFile file : files) {
-			    String details = file.getName();
-			    if (file.isDirectory()) {
-			        details = "[" + details + "]";
-			    }
-			    details += "\t\t" + file.getSize();
-			    details += "\t\t" + dateFormater.format(file.getTimestamp().getTime());
-			    System.out.println(details);
+				String details = file.getName();
+
+				details += "\t\t" + file.getSize();
+				details += "\t\t" + dateFormater.format(file.getTimestamp().getTime());
+				System.out.println(details);
 			}
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			try {
+				if (ftpClient.isConnected()) {
+					ftpClient.logout();
+					ftpClient.disconnect();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
 		}
-		
 	}
 
+	public void download() {
+		String serverFtp = "192.168.15.2";
+		int portFtp = 21;
+		String userFtp = "gma";
+		String pswFtp = "123";
+
+		FTPClient ftpClient = new FTPClient();
+		try {
+
+			ftpClient.connect(serverFtp, portFtp);
+			ftpClient.login(userFtp, pswFtp);
+			ftpClient.enterLocalPassiveMode();
+			ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+
+			// APPROACH #1: using retrieveFile(String, OutputStream)
+			String remoteFile1 = "Funcao-20190601.csv";
+			String localFile1 = "Funcao.csv";
+			File downloadFile1 = new File("E:/ftp/dados/"+localFile1 );
+			OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadFile1));
+			boolean success = ftpClient.retrieveFile(remoteFile1, outputStream1);
+			outputStream1.close();
+
+			if (success) {
+				System.out.println("Sucesso download");
+			}
+
+			/*
+			// APPROACH #2: using InputStream retrieveFileStream(String)
+			String remoteFile2 = "/test/song.mp3";
+			File downloadFile2 = new File("D:/Downloads/song.mp3");
+			OutputStream outputStream2 = new BufferedOutputStream(new FileOutputStream(downloadFile2));
+			InputStream inputStream = ftpClient.retrieveFileStream(remoteFile2);
+			byte[] bytesArray = new byte[4096];
+			int bytesRead = -1;
+			while ((bytesRead = inputStream.read(bytesArray)) != -1) {
+				outputStream2.write(bytesArray, 0, bytesRead);
+			}
+
+			success = ftpClient.completePendingCommand();
+			if (success) {
+				System.out.println("File #2 has been downloaded successfully.");
+			}
+			
+			outputStream2.close();
+			inputStream.close();
+			*/
+
+		} catch (IOException ex) {
+			System.out.println("Error: " + ex.getMessage());
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (ftpClient.isConnected()) {
+					ftpClient.logout();
+					ftpClient.disconnect();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+
+	}
 }
--- src/main/java/br/com/gma/transportearquivo/Principal.java
+++ src/main/java/br/com/gma/transportearquivo/Principal.java
@@ -1,5 +1,8 @@
 package br.com.gma.transportearquivo;
 
+
+//https://www.codejava.net/java-se/ftp/java-ftp-list-files-and-directories-example
+
 public class Principal {
 
 	public static void main(String[] args) {
@@ -7,7 +10,8 @@ public class Principal {
 		System.out.println("Inicio: "+Principal.class.getName());
 
 		Arquivo arquivo = new Arquivo();
-		arquivo.listar();
+		//arquivo.listar();
+		arquivo.download();
 	}
 
 }
